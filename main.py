4b3a93ba088f9c11e19dcfecdda36523a86b1c20
Diego-Alreaver <diego8558@hotmail.com>
2024-08-06 19:43:26 -0400

    Se agregaron dos clases nuevas

--- main.py
+++ main.py
@@ -1,9 +1,26 @@
 from tkinter import Tk, BOTH, Canvas
-from window import Window
+from window import Window, Point, Line
+
 
 def main():
-    win = Window(800, 600)
-    win.wait_for_close()
+    # Crear una ventana
+    window = Window(800, 600)
 
+    # Crear algunos puntos y líneas
+    p1 = Point(100, 200)
+    p2 = Point(300, 400)
+    p3 = Point(500, 600)
+    p4 = Point(300, 200)
+    
+
+    line1 = Line(p1, p2)
+    line2 = Line(p3, p4)
+
+    # Dibujar líneas en la ventana
+    window.draw_line(line1, "black")
+    window.draw_line(line2, "red")
+
+    # Esperar a que se cierre la ventana
+    window.wait_for_close()
 if __name__ == "__main__":
     main()
