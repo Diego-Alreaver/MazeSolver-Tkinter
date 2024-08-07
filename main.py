9e1a0033f1810e1df41e05fd5eb25fb28eeb030e
Diego-Alreaver <diego8558@hotmail.com>
2024-08-07 14:27:34 -0400

    Se agregaron celdas

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from tkinter import Tk, BOTH, Canvas
-from window import Window, Point, Line
+from window import Window, Point, Line, Cell
 
 
 def main():
@@ -12,13 +12,16 @@ def main():
     p3 = Point(500, 600)
     p4 = Point(300, 200)
     
-
     line1 = Line(p1, p2)
     line2 = Line(p3, p4)
+
+    cell = Cell(200, 50, 100, 300, window)
+    cell.draw()
 
     # Dibujar líneas en la ventana
     window.draw_line(line1, "black")
     window.draw_line(line2, "red")
+
 
     # Esperar a que se cierre la ventana
     window.wait_for_close()
--- window.py
+++ window.py
@@ -49,3 +49,25 @@ class Line:
             fill=fill_color, 
             width=2)
 
+class Cell:
+    def __init__(self, x1, y1, x2, y2, win, has_left_wall=True, has_right_wall=True, has_top_wall=True, has_bottom_wall=True):
+        self.has_left_wall = has_left_wall
+        self.has_right_wall = has_right_wall
+        self.has_top_wall = has_top_wall
+        self.has_bottom_wall = has_bottom_wall
+        self._x1 = x1
+        self._x2 = x2
+        self._y1 = y1
+        self._y2 = y2
+        self._win = win
+
+    def draw(self):
+        if self.has_left_wall:
+            self._win.canvas.create_line(self._x1, self._y1, self._x1, self._y2)
+        if self.has_right_wall:
+            self._win.canvas.create_line(self._x2, self._y1, self._x2, self._y2)
+        if self.has_top_wall:
+            self._win.canvas.create_line(self._x1, self._y1, self._x2, self._y1)
+        if self.has_bottom_wall:
+            self._win.canvas.create_line(self._x1, self._y2, self._x2, self._y2)
+
