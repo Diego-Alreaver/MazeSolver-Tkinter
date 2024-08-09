66be97e2da457a05c6ef9f8dca21bf6a100b29b4
Diego-Alreaver <diego8558@hotmail.com>
2024-08-09 12:44:02 -0400

    celdas visitadas

--- test.py
+++ test.py
@@ -9,10 +9,7 @@ class Tests(unittest.TestCase):
     def test_maze_create_cells(self):
         num_cols = 12
         num_rows = 10
-        screen_x = 800
-        screen_y = 600
-        win = Window(screen_x, screen_y)
-        m1 = Maze(0, 0, num_rows, num_cols, 10, 10, win)
+        m1 = Maze(0, 0, num_rows, num_cols, 10, 10)
         self.assertEqual(
             len(m1._cells),
             num_cols,
@@ -22,5 +19,19 @@ class Tests(unittest.TestCase):
             num_rows,
         )
 
+    def test_maze_break_entrance_and_exit(self):
+        num_cols = 12
+        num_rows = 10
+        m1 = Maze(0, 0, num_rows, num_cols, 10, 10)
+        self.assertEqual(
+            m1._cells[0][0].has_top_wall,
+            False,
+        )
+        self.assertEqual(
+            m1._cells[num_cols - 1][num_rows - 1].has_bottom_wall,
+            False,
+        )
+
+
 if __name__ == "__main__":
-    unittest.main() 
+    unittest.main()
